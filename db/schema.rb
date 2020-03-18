4a61a66f9fa1657d8372c4dadaf628e1baffc807
manhcuongdtbk <manhcuongdtbk@gmail.com>
2020-03-18 23:34:50 +0700

    Getting Started with Rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_044938) do
+ActiveRecord::Schema.define(version: 2020_03_18_162446) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
